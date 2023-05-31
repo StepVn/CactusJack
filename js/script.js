47d757115a9a93f8ef08ae4bfca2cb722e5dcce4
Stepan <114872044+Showmeyourbobs@users.noreply.github.com>
2023-05-31 18:12:15 +0700

    add user question

--- js/script.js
+++ js/script.js
@@ -10,12 +10,28 @@ const  personalMovieDB = {
     privat: false
 };
 
-const question = prompt('Один из послдених просмотренных фильмов?', ''),
-        question2 = prompt('На сколько оцените его?', ''),
-        question3 = prompt('Один из послдених просмотренных фильмов?', ''),
-        question4 = prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+    const question = prompt('Один из послдених просмотренных фильмов?', ''),
+          question2 = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[question] = question2;
-personalMovieDB.movies[question3] = question4;
+    if (question != null && question2 != null && question != '' && question2 != '' && question.length < 50) {
+        personalMovieDB.movies[question] = question2;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10 ) {
+    console.log('Просмотрено довольно мало фильмов')
+} else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель')
+} else if(personalMovieDB.count >= 30){
+    console.log('Вы киноман')
+} else {
+    console.log('Произошла ошибка')
+}
 
 console.log(personalMovieDB);
+
